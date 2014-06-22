860df0fd78f619536d7c515dabd25e305a9a19d6
Piero Wbmstr <me@e-piwi.fr>
2014-06-22 14:10:52 +0200

    cleanup & review of sources

--- src/Validator/AbstractMasksValidatorHelper.php
+++ src/Validator/AbstractMasksValidatorHelper.php
@@ -12,64 +12,61 @@ namespace Validator;
 /**
  * The application validator model
  *
- * @author 		Piero Wbmstr <me@e-piwi.fr>
+ * @author  Piero Wbmstr <me@e-piwi.fr>
  */
 abstract class AbstractMasksValidatorHelper
 {
 
-	/**
-	 * A set of masks to registered for validators use
-	 */
-	protected $masks;
+    /**
+     * @var array A set of masks to registered for validators use
+     */
+    protected $masks;
 
-	/**
-	 * Register a new mask set
-	 *
-	 * @param array $masks The mask table as name=>mask pairs
-	 *
-	 * @return object $this for method chaining
-	 */
-	public function setMasks($masks)
-	{
-		$this->masks = $mask;
-		return $this;
-	}
+    /**
+     * Register a new mask set
+     *
+     * @param   array $masks The mask table as name=>mask pairs
+     * @return  self
+     */
+    public function setMasks($masks)
+    {
+        $this->masks = $masks;
+        return $this;
+    }
 
-	/**
-	 * Register a new mask
-	 *
-	 * @param string $mask_name The mask name to set
-	 * @param string $mask The mask value to set
-	 *
-	 * @return object $this for method chaining
-	 */
-	public function addMask($mask_name, $mask)
-	{
-		$this->masks[$mask_name] = $mask;
-		return $this;
-	}
+    /**
+     * Register a new mask
+     *
+     * @param   string $mask_name The mask name to set
+     * @param   string $mask The mask value to set
+     * @return  self
+     */
+    public function addMask($mask_name, $mask)
+    {
+        $this->masks[$mask_name] = $mask;
+        return $this;
+    }
 
-	/**
-	 * Get the masks registry
-	 *
-	 * @return array The masks array
-	 */
-	public function getMasks()
-	{
-		return $this->masks;
-	}
+    /**
+     * Get the masks registry
+     *
+     * @return array The masks array
+     */
+    public function getMasks()
+    {
+        return $this->masks;
+    }
 
-	/**
-	 * Get a registered mask
-	 *
-	 * @param string $mask_name The name of the mask to get
-	 *
-	 * @return string The value registered for the mask name, an empty string if it has not been found
-	 */
-	public function getMask($mask_name)
-	{
-		return isset($this->masks[$mask_name]) ? $this->masks[$mask_name] : '';
-	}
+    /**
+     * Get a registered mask
+     *
+     * @param   string $mask_name The name of the mask to get
+     * @return  string The value registered for the mask name, an empty string if it has not been found
+     */
+    public function getMask($mask_name)
+    {
+        return isset($this->masks[$mask_name]) ? $this->masks[$mask_name] : '';
+    }
 
 }
 
--- src/Validator/InternetProtocolValidator.php
+++ src/Validator/InternetProtocolValidator.php
@@ -12,93 +12,95 @@ namespace Validator;
 /**
  * IP address validator
  *
- * @author 		Piero Wbmstr <me@e-piwi.fr>
+ * @author  Piero Wbmstr <me@e-piwi.fr>
  */
 class InternetProtocolValidator
     extends AbstractMasksValidatorHelper
     implements ValidatorInterface
 {
 
-	/**
-	 * The version of the Internet Protocol to validate
-	 */
-	 protected $version='v4';
+    /**
+     * The version of the Internet Protocol to validate
+     */
+     protected $version='v4';
 
-	/**
-	 * Constructor
-	 *
-	 * @param str $mask The mask to test
-	 * @param str $ref The mask reference
-	 */
-	public function __construct( $version=null )
-	{
-		$this->addMask('v4', '(?:[0-9]{1,3}\.){3}[0-9]{1,3}');
-		$this->addMask('v4_full', '^' . $this->getMask('v4') . '$');
+    /**
+     * Constructor
+     *
+     * @param string $version
+     */
+    public function __construct( $version=null )
+    {
+        $this->addMask('v4', '(?:[0-9]{1,3}\.){3}[0-9]{1,3}');
+        $this->addMask('v4_full', '^' . $this->getMask('v4') . '$');
 
-		$this->addMask('v6', '(((?=.*(::))(?!.*\3.+\3))\3?|([\dA-F]{1,4}(\3|:\b|$)|\2))(?4){5}((?4){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})\z');
-		$this->addMask('v6_full', '^' . $this->getMask('v6') );
+        $this->addMask('v6', '(((?=.*(::))(?!.*\3.+\3))\3?|([\dA-F]{1,4}(\3|:\b|$)|\2))(?4){5}((?4){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})\z');
+        $this->addMask('v6_full', '^' . $this->getMask('v6') );
 
-		if (!is_null($version)) $this->setVersion($version);
-	}
+        if (!is_null($version)) $this->setVersion($version);
+    }
 
-	/**
-	 * Set the version of the protocol to use
-	 *
-	 * @param string $version The version number of the protocol, must be a constructor's masks reference
-	 * @return object $this for method chaining
-	 */
-	public function setVersion($version)
-	{
-		$this->version = $version;
-		return $this;
-	}
+    /**
+     * Set the version of the protocol to use
+     *
+     * @param string $version The version number of the protocol, must be a constructor's masks reference
+     * @return object $this for method chaining
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+        return $this;
+    }
 
-	/**
-	 * Process validation
-	 *
-	 * @param string $value The string to validate
-	 * @param bool $send_errors Does the function must throw exceptions on validation failures ?
-	 * @return bool TRUE if $value pass the length validation
-	 */
-	public function validate($value, $send_errors = false)
-	{
-		// the mask validation
-		$full_mask = $this->getMask( $this->version.'_full' );
-		$mask = $this->getMask( $this->version );
-		if (!strlen( $mask )) {
-			if (true===$send_errors) {
-				throw new \Exception(
-					sprintf("No mask defined in InternetProtocol validator for version %s!", $this->version)
-				);
-			}
-			return false;
-		}
+    /**
+     * Process validation
+     *
+     * @param   string  $value          The local part of an email address to validate
+     * @param   bool    $send_errors    Does the function must throw exceptions on validation failures ?
+     * @return  bool    TRUE if `$value` pass the Email validation
+     * @throws  \Exception for each invalid part if `$send_errors` is true
+     */
+    public function validate($value, $send_errors = false)
+    {
+        // the mask validation
+        $full_mask = $this->getMask( $this->version.'_full' );
+        $mask = $this->getMask( $this->version );
+        if (!strlen( $mask )) {
+            if (true===$send_errors) {
+                throw new \Exception(
+                    sprintf("No mask defined in InternetProtocol validator for version %s!", $this->version)
+                );
+            }
+            return false;
+        }
 
-		// if we have a 'full' mask, we try it in priority
-		$validator = new StringMaskValidator( 
-			strlen($full_mask) ? $full_mask : '^' . $mask . '$', 
-			'IP' . $this->version
-		);
+        // if we have a 'full' mask, we try it in priority
+        $validator = new StringMaskValidator(
+            strlen($full_mask) ? $full_mask : '^' . $mask . '$',
+            'IP' . $this->version
+        );
 
-		// does $value pass the mask
-		if (false===$ok = $validator->validate($value, false)) {
-			if (true===$send_errors) {
-				throw new \Exception(
-					sprintf('String [%s] is not a valid InternetProtocol address in version %s', $value, $this->version)
-				);
-			}
-			return false;
-		}
+        // does $value pass the mask
+        try {
+            if (false===$ok = $validator->validate($value, false)) {
+                if (true===$send_errors) {
+                    throw new \Exception(
+                        sprintf('String [%s] is not a valid InternetProtocol address in version %s', $value, $this->version)
+                    );
+                }
+                return false;
+            }
+        } catch (\Exception $e) { throw $e; }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Try to make $value pass the validation
-	 */
-	public function sanitize($value)
-	{
-	}
+    /**
+     * Try to make $value pass the validation
+     */
+    public function sanitize($value)
+    {
+    }
 
 }
 
@@ -107,22 +109,22 @@ class InternetProtocolValidator
 /*
 //Valid IP v4
 $ipv4_ok = array(
-	'192.168.0.2',
-	'127.0.0.1'
+    '192.168.0.2',
+    '127.0.0.1'
 );
 //Valid IP v6
 $ipv6_ok = array(
-	'2001:db8:1ff::a0b:dbd0',
+    '2001:db8:1ff::a0b:dbd0',
 );
 //Invalid IP
 $ip_notok = array(
-	'example.com',
-	'192.168.0.2.9876',
-	'127.0.0.az',
-	'127.0.az.1',
-	'127.0.0az.1',
-	'127.0.0.az.1',
-	'2001:db8:::::a0b:dbd0',
+    'example.com',
+    '192.168.0.2.9876',
+    '127.0.0.az',
+    '127.0.az.1',
+    '127.0.0az.1',
+    '127.0.0.az.1',
+    '2001:db8:::::a0b:dbd0',
 );
 
 $unit_test = CarteBlanche::getContainer()->get('unit_test');
@@ -132,46 +134,46 @@ $verbose = false;
 $v = new \Lib\Validator\InternetProtocolValidator();
 
 $unit_test->newTestGroup( 
-	'Strings that must pass validation of the "%s" class for version 4.', 
-	'\Lib\Validator\InternetProtocolValidator',
-	'mlkj'
+    'Strings that must pass validation of the "%s" class for version 4.',
+    '\Lib\Validator\InternetProtocolValidator',
+    'mlkj'
 );
 foreach( $ipv4_ok as $_str )
 {
-	$unit_test->assertTrue(
-		$v->validate( $_str, $verbose ),
-		sprintf("IPv4 validation on string [<em>%s</em>]", $_str)
-	);
+    $unit_test->assertTrue(
+        $v->validate( $_str, $verbose ),
+        sprintf("IPv4 validation on string [<em>%s</em>]", $_str)
+    );
 }
 
 $v->setVersion('v6');
 $unit_test->newTestGroup( 'Strings that must pass validation of the "%s" class for version 6.', '\Lib\Validator\InternetProtocolValidator' );
 foreach( $ipv6_ok as $_str )
 {
-	$unit_test->assertTrue(
-		$v->validate( $_str, $verbose ),
-		sprintf("IPv6 validation on string [<em>%s</em>]", $_str)
-	);
+    $unit_test->assertTrue(
+        $v->validate( $_str, $verbose ),
+        sprintf("IPv6 validation on string [<em>%s</em>]", $_str)
+    );
 }
 
 $v->setVersion('v4');
 $unit_test->newTestGroup( 'Strings that must NOT pass validation of the "%s" class for version 4.', '\Lib\Validator\InternetProtocolValidator' );
 foreach( $ip_notok as $_str )
 {
-	$unit_test->assertFalse(
-		$v->validate( $_str, $verbose ),
-		sprintf("IP validation on string [<em>%s</em>]", $_str)
-	);
+    $unit_test->assertFalse(
+        $v->validate( $_str, $verbose ),
+        sprintf("IP validation on string [<em>%s</em>]", $_str)
+    );
 }
 
 $v->setVersion('v6');
 $unit_test->newTestGroup( 'Strings that must NOT pass validation of the "%s" class for version 6.', '\Lib\Validator\InternetProtocolValidator' );
 foreach( $ip_notok as $_str )
 {
-	$unit_test->assertFalse(
-		$v->validate( $_str, $verbose ),
-		sprintf("IP validation on string [<em>%s</em>]", $_str)
-	);
+    $unit_test->assertFalse(
+        $v->validate( $_str, $verbose ),
+        sprintf("IP validation on string [<em>%s</em>]", $_str)
+    );
 }
 
 echo $unit_test;
